75a828d408af6d8066a74ee29349e61dfbd8b5d3
Danyl Fernandes <realllydan@gmail.com>
2021-08-16 10:17:32 -0700

    :construction: All cross origins on the server

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import data as data
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:1234",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def index():
