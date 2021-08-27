90486065c0a16ffd92b9a42d541ca2cac671cd29
Danyl Fernandes <realllydan@gmail.com>
2021-08-27 03:09:46 -0700

    :lipstick: Add functions for today, t+1 and t+2

--- data.py
+++ data.py
@@ -1,22 +1,69 @@
 import scraper
 import helper
+import time
 
 data = scraper.get_all_data()
 
 
+def is_before_today(date_string):
+    if date_string == '':
+        return True
+    today = time.strptime(helper.get_date_today(), "%d-%b-%Y")
+    day = time.strptime(date_string, "%d-%b-%Y")
+    return day < today
+
+
 def all_course_work():
     all_course_work = []
+
     for row in data[1:]:
-        all_course_work.append(helper.get_row_with_keys(row))
+        if not is_before_today(row[6]):
+            all_course_work.append(helper.get_row_with_keys(row))
     return all_course_work
 
 
 def all_assignments():
     all_assignments = []
+
     for row in data[1:]:
         if row[2] == 'Assignment':
             all_assignments.append(helper.get_row_with_keys(row))
     return all_assignments
+
+
+def all_due_today():
+    all_work_due_today = []
+    all_work = all_course_work()
+    today = helper.get_date_today()
+
+    for row in all_work:
+        if row['date_due'] == today:
+            all_work_due_today.append(row)
+    return all_work_due_today
+
+
+def all_due_tommorrow():
+    all_work_due_tommorrow = []
+    all_work = all_course_work()
+    tommorrow = helper.get_date_tommorrow()
+
+    for row in all_work:
+        if row['date_due'] == tommorrow:
+            all_work_due_tommorrow.append(row)
+    return all_work_due_tommorrow
+
+
+def all_due_after_tommorow():
+    all_work_due_after_tommorow = []
+    all_work = all_course_work()
+
+    today = helper.get_date_today()
+    tommorrow = helper.get_date_tommorrow()
+
+    for row in all_work:
+        if row['date_due'] != today and row['date_due'] != tommorrow:
+            all_work_due_after_tommorow.append(row)
+    return all_work_due_after_tommorow
 
 
 def all_experiments():
