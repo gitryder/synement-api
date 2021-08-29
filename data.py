c69f5ca35be4b870dd8462a44e8244cff09e1b41
Danyl Fernandes <realllydan@gmail.com>
2021-08-29 08:01:15 -0700

    :lipstick: Get table data  dynamically by year

--- data.py
+++ data.py
@@ -2,7 +2,9 @@ import scraper
 import helper
 import time
 
+se_data = scraper.get_all_data_for("SE")
 te_data = scraper.get_all_data_for("TE")
+be_data = scraper.get_all_data_for("BE")
 
 
 def is_before_today(date_string):
@@ -13,27 +15,25 @@ def is_before_today(date_string):
     return day < today
 
 
-def all_course_work():
+def all_course_work(year):
+    if (year == "se"):
+        data = se_data
+    elif (year == "be"):
+        data = be_data
+    else:
+        data = te_data
+
     all_course_work = []
 
-    for row in te_data[1:]:
+    for row in data[1:]:
         if not is_before_today(row[6]):
             all_course_work.append(helper.get_row_with_keys(row))
     return all_course_work
 
 
-def all_assignments():
-    all_assignments = []
-
-    for row in te_data[1:]:
-        if row[2] == 'Assignment':
-            all_assignments.append(helper.get_row_with_keys(row))
-    return all_assignments
-
-
-def all_due_today():
+def all_due_today(year):
     all_work_due_today = []
-    all_work = all_course_work()
+    all_work = all_course_work(year)
     today = helper.get_date_today()
 
     for row in all_work:
@@ -42,9 +42,9 @@ def all_due_today():
     return all_work_due_today
 
 
-def all_due_tomorrow():
+def all_due_tomorrow(year):
     all_work_due_tomorrow = []
-    all_work = all_course_work()
+    all_work = all_course_work(year)
     tomorrow = helper.get_date_tomorrow()
 
     for row in all_work:
@@ -53,9 +53,9 @@ def all_due_tomorrow():
     return all_work_due_tomorrow
 
 
-def all_due_after_tomorrow():
+def all_due_after_tomorrow(year):
     all_work_due_after_tomorrow = []
-    all_work = all_course_work()
+    all_work = all_course_work(year)
 
     today = helper.get_date_today()
     tomorrow = helper.get_date_tomorrow()
@@ -64,6 +64,17 @@ def all_due_after_tomorrow():
         if row['date_due'] != today and row['date_due'] != tomorrow:
             all_work_due_after_tomorrow.append(row)
     return all_work_due_after_tomorrow
+
+
+# Unused methods
+
+def all_assignments():
+    all_assignments = []
+
+    for row in te_data[1:]:
+        if row[2] == 'Assignment':
+            all_assignments.append(helper.get_row_with_keys(row))
+    return all_assignments
 
 
 def all_experiments():
