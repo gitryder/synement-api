8f52233237635f556a22e977d16271b4ed95e7a4
Danyl Fernandes <realllydan@gmail.com>
2021-09-11 00:31:49 +0530

    Add scraping for additional table

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import requests
 
 session = requests.Session()
 
-def get_table_for(year=""):
+def get_tables_for(year=""):
     if (year == "se"):
         username =  os.environ.get('SYNE_SE_USER')
         password =  os.environ.get('SYNE_SE_PASS')
@@ -19,23 +19,28 @@ def get_table_for(year=""):
     payload = {'AY_ID': 21, 'UserName': username, 'Password': password}
 
     s = session.post("https://xavier.qualcampus.com/Account/LogOn", data=payload)
+
     s = session.get('https://xavier.qualcampus.com/Dashboard_Assignment_Student')
-
     soup = BeautifulSoup(s.text, 'html.parser')
-    table = soup.find('table', attrs={'id': 'AssignmentListTable'})
+    asg_table = soup.find('table', attrs={'id': 'AssignmentListTable'})
 
-    return table
+    s = session.get('https://xavier.qualcampus.com/Dashboard_Quiz_Student')
+    soup = BeautifulSoup(s.text, 'html.parser')
+    quiz_table = soup.find('table', attrs={'id': 'QuizSummaryTbl'})
+
+    return [asg_table, quiz_table]
 
 
 def get_all_data_for(year):
-    table = get_table_for(year)
+    tables = get_tables_for(year)
 
     rows = []
-    trs = table.find_all('tr')
-    headerow = [td.get_text(strip=True) for td in trs[0].find_all('th')]
-    if headerow:
-        rows.append(headerow)
-        trs = trs[1:]
-    for tr in trs:
-        rows.append([td.get_text(strip=True) for td in tr.find_all('td')])
+    for table in tables:
+        trs = table.find_all('tr')
+        headerow = [td.get_text(strip=True) for td in trs[0].find_all('th')]
+        if headerow:
+            rows.append(headerow)
+            trs = trs[1:]
+        for tr in trs:
+            rows.append([td.get_text(strip=True) for td in tr.find_all('td')])
     return helper.clean_data(rows)
