236d054c9e9d92e331b400742b36b929e94e15d6
Danyl Fernandes <androidan1304@gmail.com>
2021-09-27 17:40:48 +0530

    :bug: Fix indentation errors

--- updater.py
+++ updater.py
@@ -4,14 +4,17 @@ import helper
 
 sched = BlockingScheduler()
 
+
 @sched.scheduled_job('interval', minutes=120)
 def fetch_data():
-    se_data = scraper.get_all_data_for('se')
-    te_data = scraper.get_all_data_for('te')
-    be_data = scraper.get_all_data_for('be')
+  print("Fetching data")
+  se_data = scraper.get_all_data_for('se')
+  te_data = scraper.get_all_data_for('te')
+  be_data = scraper.get_all_data_for('be')
 
-    helper.serialize_to_file('se_data', se_data)
-    helper.serialize_to_file('te_data', te_data)
-    helper.serialize_to_file('be_data', be_data)
+  print("Serializing fetched data to files")
+  helper.serialize_to_file('se_data', se_data)
+  helper.serialize_to_file('te_data', te_data)
+  helper.serialize_to_file('be_data', be_data)
 
 sched.start()
